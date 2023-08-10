598652af7aa0c9118b4c6a44e23e0d459f18f99a
rschuquel <renan.schuquel@unimedvtrp.com.br>
2023-08-10 13:29:32 -0300

    Trying to get the play points

--- AoC2022/day2/main.c
+++ AoC2022/day2/main.c
@@ -3,8 +3,32 @@
 #include <string.h>
 #include <stdbool.h>
 
+int get_play_point(char play[3]){
+    char possible_plays[9][3] = {
+        "A X", "A Y", "A Z",
+        "B X", "B Y", "B Z",
+        "C X", "C Y", "C Z",
+    };
+
+    int results_points[9] = {
+        3, 6, 0,
+        6, 3, 0,
+        0, 6, 3
+    };
+
+    printf("Play: %s\n", play);
+    for(int i = 0; i < sizeof(possible_plays); i++){
+        printf("Current play looked: %s\n", possible_plays[i]);
+
+        if(play == possible_plays[i]){
+            return results_points[i];
+        }
+    }
+}
+
 int main(){
     FILE *fptr;
+
     fptr = fopen("input.txt", "r");
 
     if(fptr == NULL){
@@ -12,33 +36,32 @@ int main(){
         exit(1);
     }
 
-    int max_line_size = 4;
-    char line[max_line_size];
+    char *my_play = malloc(1);
 
     int index = 0;
 
+    int max_line_size = 4;
+    char line[max_line_size];
+
     while(fgets(line, max_line_size, fptr)){
+        if(index > 0){
+            break;
+        }
+
         bool isBreakLine = line[0] == '\n';
         if(isBreakLine){
             continue;
         }
 
-        printf("Line: %s\n", line);
-
-        char *oponnent_play = malloc(1);
-        strcpy(oponnent_play, &line[0]);
-
-        char *my_play = malloc(1);
         strcpy(my_play, &line[2]);
 
-        printf("Opponent play: %c\n", *oponnent_play);
-        printf("My play: %c\n\n", *my_play);
+        int result_point = get_play_point(line);
+
+        printf("My point on this play: %d", result_point);
 
         index++;
-
-        free(oponnent_play);
-        free(my_play);
     }
 
+    free(my_play);
     return 0;
 }
